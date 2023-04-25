77d0369370865354322f9401b4a3a31abc05ad55
Xavier Dunkley <xavierxv18@gmail.com>
2023-04-25 03:59:31 -0400

    made changes to routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,28 +2,20 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    category = Category.all
-    category.to_json
+  get "/categories" do
+    categories = Category.all
+    categories.to_json(:include => :tasks)
   end
 
-  post '/task' do
-    category = Category.find_or_create_by(name:params["Category"])
-    task=Task.create(name: params["Task"], category:category)
-    Task.all.to_json
-  end
-
-  patch '/task/:id' do
-    category = Category.find(params[:id])
-    category.update(
-      task: params[:task]
-    )
-    category.to_json
-  end
-
-  delete '/task/:id' do
+  delete '/category/:id' do
     category = Category.find(params[:id])
     category.destroy
+    category.to_json
+  end
+
+  post '/category/:name' do
+    category = Category.new(name: params["name"])
+    category.save
     category.to_json
   end
 
